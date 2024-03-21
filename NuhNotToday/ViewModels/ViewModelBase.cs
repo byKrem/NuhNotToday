07b4601e9c2ff17548e9ed18198e9b73f1d80e2b
Rubanov <bykr3m@gmail.com>
2024-03-21 12:22:13 +0300

    Init Commit

---
+++ NuhNotToday/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace NuhNotToday.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ NuhNotToday/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace NuhNotToday.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
